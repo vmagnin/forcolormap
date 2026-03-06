c4324e21c29e78213d7b33f30b660c0231a9388c
vmagnin <vincent.magnin@libertysurf.fr>
2026-03-06 11:22:31 +0100

    docs: ppm2png.sh => small improvements

--- colormaps_list/ppm2png.sh
+++ colormaps_list/ppm2png.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# Requires convert program from ImageMagick
+# Requires ImageMagick
 
 # Ensure output directory exists
 mkdir -p png
+# Cleanup
+rm png/*.png
 
+# Converting each ppm file to png
 for ppm_file in ppm/*.ppm; do
     if [ -e "$ppm_file" ]; then
         png_file="png/$(basename "$ppm_file" .ppm).png"
         echo "Converting $ppm_file to $png_file"
-        convert "$ppm_file" "$png_file"
+        magick "$ppm_file" "$png_file"
     fi
 done
 
